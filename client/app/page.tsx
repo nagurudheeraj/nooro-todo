38fedf8349bd267889f8ca4811f7dec7392277ef
Dheeraj Naguru <Dheeraj@Mac-84.lan>
2024-12-22 16:09:28 -0600

    Added meta data

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,18 @@
 import Content from "@/app/components/Content";
 import CreateTodo from "./components/CreateTodo";
-import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Todo App",
+  description:
+    "A simple and effective Todo application designed to help users organize and track their to-do lists and  built using Next.js, Express.js, TypeScript, and MySQL. It leverages Clever Cloud for hosting the MySQL database.",
+};
 
 const Home: React.FC = () => {
   return (
     <>
       <section className="todoapp relative w-full min-h-screen flex flex-col bg-[#1a1a1a]">
-        <CreateTodo /> 
+        <CreateTodo />
         <Content />
       </section>
     </>
